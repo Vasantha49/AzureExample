a8205dade73a9bf401b969ebab9a2eb172d3b43e
Vasantha Kumari Kommanapalli <vasantha.thechamp@gmail.com>
2026-03-06 22:37:04 +0100

    Initial Spring Boot App

---
+++ src/main/java/org/example/azureexample/AzureExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.azureexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/azureexample/Controller/UserController.java
@@ -0,0 +1,26 @@
+package org.example.azureexample.Controller;
+
+import org.example.azureexample.Entity.User;
+import org.example.azureexample.Repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repo;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return repo.save(user);
+    }
+}
+
